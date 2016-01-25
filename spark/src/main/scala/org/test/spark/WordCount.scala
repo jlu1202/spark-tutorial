09064c3cb7c9004ec9ab77f9a6721ee860d6e807
jlu1202 <jlu1202@gmail.com>
2016-01-24 21:18:39 -0500

    first commit

---
+++ spark/src/main/scala/org/test/spark/WordCount.scala
@@ -0,0 +1,23 @@
+package org.test.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  def main(args: Array[String]) = {
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local[2]")
+    
+    val sc = new SparkContext(conf)
+    
+    val test = sc.textFile("food.txt")
+    test.flatMap { line => line.split(" ") }
+        .map { word => (word, 1) }
+        .reduceByKey(_ + _)
+        .saveAsTextFile("food.counts.txt")
+        
+    //Stop the Spark context
+    sc.stop
+  }
+}
